6e02fdbc9e42b081d617817882ce82727e144002
lake.zero <lake.zero@kakaostyle.com>
2025-06-29 22:09:11 +0900

    feat: add kakao map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,18 @@
-import { Button, HStack, Container, Text } from "@chakra-ui/react";
-import { useGoogleAuth } from "../hooks/useGoogleAuth";
-import { useGetMe } from "@/hooks/useGetMe";
-import { useGitHubAuth } from "@/hooks/useGitHubAuth";
+import { Box } from "@chakra-ui/react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function Home() {
-  const { login } = useGoogleAuth();
-  const { login: loginGithub } = useGitHubAuth();
-  const { data: me } = useGetMe();
-
   return (
-    <Container
-      bg="white"
-      h="100vh"
-      w="100vw"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <HStack gap={4}>
-        <Button color="white" bg="blue.500" onClick={login}>
-          Google Login
-        </Button>
-        <Button color="white" bg="green.500" onClick={loginGithub}>
-          Github Login
-        </Button>
-      </HStack>
-      <Text>{me?.data.user.name}</Text>
-    </Container>
+    <Box bg="white" display="flex" w="full" h="full">
+      <Map
+        center={{ lat: 37.394946, lng: 127.110828 }}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <MapMarker position={{ lat: 37.394946, lng: 127.110828 }}>
+          <div style={{ color: "#000" }}>판교역</div>
+        </MapMarker>
+      </Map>
+    </Box>
   );
 }
 
