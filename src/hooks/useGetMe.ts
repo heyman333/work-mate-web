cb197ebe025004a2c5e7b6c2c14d7c504d31af7d
lake.zero <lake.zero@kakaostyle.com>
2025-06-30 23:13:52 +0900

    feat: 타입 자동생성

--- src/hooks/useGetMe.ts
+++ src/hooks/useGetMe.ts
@@ -1,9 +1,11 @@
 import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 import api from "../utils/api";
-import type { User } from "../types";
 import type { AxiosResponse } from "axios";
+import type { SchemaUser } from "../types/api";
 
-export const useGetMe = (): UseQueryResult<AxiosResponse<{ user: User }>> => {
+export const useGetMe = (): UseQueryResult<
+  AxiosResponse<{ user: SchemaUser }>
+> => {
   return useQuery({
     queryKey: ["me"],
     queryFn: () => api.get("/auth/me"),
