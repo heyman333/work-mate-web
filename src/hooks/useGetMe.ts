956c3e62c82a4b8e130feb657abd56e5fbc29440
lake.zero <lake.zero@kakaostyle.com>
2025-07-05 11:07:27 +0900

    fix: fix type

--- src/hooks/useGetMe.ts
+++ src/hooks/useGetMe.ts
@@ -1,14 +1,13 @@
-import { useQuery, type UseQueryResult } from "@tanstack/react-query";
-import api from "../utils/api";
-import type { AxiosResponse } from "axios";
-import type { SchemaUser } from "../api/api";
+import { useQuery } from "@tanstack/react-query";
+import { Api } from "../api/api";
 
-export const useGetMe = (): UseQueryResult<
-  AxiosResponse<{ user: SchemaUser }>
-> => {
+export const useGetMe = () => {
   return useQuery({
     queryKey: ["me"],
-    queryFn: () => api.get("/auth/me"),
+    queryFn: () => {
+      const auth = new Api().auth;
+      return auth.getAuth();
+    },
     staleTime: Infinity,
     gcTime: Infinity,
     retry: false,
